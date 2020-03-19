fd44b43e33a65f5055c9e290dd4558c7f26deb28
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-03-19 18:28:51 +0100

    Use cats Comparison instead custom one

--- src/main/scala/rampart/Interval.scala
+++ src/main/scala/rampart/Interval.scala
@@ -1,6 +1,7 @@
 package rampart
 
 import cats.Order
+import cats.kernel.Comparison.{EqualTo, GreaterThan, LessThan}
 
 case class Interval[A: Order] private (lesser: A, greater: A)
 object Interval {
@@ -15,27 +16,27 @@ object Interval {
   def toInterval[A: Order](t: (A, A)): Interval[A] = toInterval(t._1, t._2)
 
   def relate[A: Order](x: Interval[A], y: Interval[A]): Relation = {
-    import Comparison._
+    import cats.implicits._
     import Relation._
 
-    val lxly = compare(x.lesser, y.lesser)
-    val lxgy = compare(x.lesser, y.greater)
-    val gxly = compare(x.greater, y.lesser)
-    val gxgy = compare(x.greater, y.greater)
+    val lxly = x.lesser.comparison(y.lesser)
+    val lxgy = x.lesser.comparison(y.greater)
+    val gxly = x.greater.comparison(y.lesser)
+    val gxgy = x.greater.comparison(y.greater)
 
     (lxly, lxgy, gxly, gxgy) match {
-      case (Equals, _, _, Equals)           => Equal
+      case (EqualTo, _, _, EqualTo)         => Equal
       case (_, _, LessThan, _)              => Before
-      case (_, _, Equals, _)                => Meets
-      case (_, Equals, _, _)                => MetBy
+      case (_, _, EqualTo, _)               => Meets
+      case (_, EqualTo, _, _)               => MetBy
       case (_, GreaterThan, _, _)           => After
       case (LessThan, _, _, LessThan)       => Overlaps
-      case (LessThan, _, _, Equals)         => FinishedBy
+      case (LessThan, _, _, EqualTo)        => FinishedBy
       case (LessThan, _, _, GreaterThan)    => Contains
-      case (Equals, _, _, LessThan)         => Starts
-      case (Equals, _, _, GreaterThan)      => StartedBy
+      case (EqualTo, _, _, LessThan)        => Starts
+      case (EqualTo, _, _, GreaterThan)     => StartedBy
       case (GreaterThan, _, _, LessThan)    => During
-      case (GreaterThan, _, _, Equals)      => Finishes
+      case (GreaterThan, _, _, EqualTo)     => Finishes
       case (GreaterThan, _, _, GreaterThan) => OverlappedBy
     }
   }
