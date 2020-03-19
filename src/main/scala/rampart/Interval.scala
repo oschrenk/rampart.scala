3aa7d6f2b6e7ceedc549ce6daed65fe16afe2163
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-03-19 20:20:51 +0100

    Add scaladoc

--- src/main/scala/rampart/Interval.scala
+++ src/main/scala/rampart/Interval.scala
@@ -3,6 +3,14 @@ package rampart
 import cats.Order
 import cats.kernel.Comparison.{EqualTo, GreaterThan, LessThan}
 
+/**
+  * Represents an interval bounded by two values, the 'lesser' and the 'greater'
+  * value. These values can be anything with an `cats.kernel.Order` instance:
+  * numbers, times, strings — you name it.
+  *
+  * Use `toInterval` to construct an interval
+  * Use `relate` to determine how two intervals relate to each other.
+ **/
 case class Interval[A: Order] private (lesser: A, greater: A)
 object Interval {
   import cats.implicits._
@@ -13,8 +21,34 @@ object Interval {
     else
       Interval(a, b)
 
+  /**
+    * Converts a tuple into an [[Interval]]. Note that this requires an
+    * `cats.kernel.Order` constraint so that the Interval can be sorted on
+    * construction.
+   **/
   def toInterval[A: Order](t: (A, A)): Interval[A] = toInterval(t._1, t._2)
 
+  /**
+    * Relates two [[Interval]]s. Calling `relate(x, y)` tells you how Interval
+    * `x` * relates to Interval `y`. Consult the [[Relation]] documentation
+    * for an explanation of all the possible results.
+    *
+    * {{{
+    * relate(toInterval(1, 2)), (toInterval(3, 7)) == Before
+    * relate(toInterval(2, 3)), (toInterval(3, 7)) == Meets
+    * relate(toInterval(2, 4)), (toInterval(3, 7)) == Overlaps
+    * relate(toInterval(2, 7)), (toInterval(3, 7)) == FinishedBy
+    * relate(toInterval(2, 8)), (toInterval(3, 7)) == Contains
+    * relate(toInterval(3, 4)), (toInterval(3, 7)) == Starts
+    * relate(toInterval(3, 7)), (toInterval(3, 7)) == Equal
+    * relate(toInterval(3, 8)), (toInterval(3, 7)) == StartedBy
+    * relate(toInterval(4, 6)), (toInterval(3, 7)) == During
+    * relate(toInterval(6, 7)), (toInterval(3, 7)) == Finishes
+    * relate(toInterval(6, 8)), (toInterval(3, 7)) == OverlappedBy
+    * relate(toInterval(7, 8)), (toInterval(3, 7)) == MetBy
+    * relate(toInterval(8, 9)), (toInterval(3, 7)) == After
+    * }}}
+   **/
   def relate[A: Order](x: Interval[A], y: Interval[A]): Relation = {
     import cats.implicits._
     import Relation._
--- src/main/scala/rampart/Relation.scala
+++ src/main/scala/rampart/Relation.scala
@@ -1,20 +1,257 @@
 package rampart
 
+/**
+  * Describes how two [[Interval]]s relate to each other. There
+  * are 13 possible relations. Taken together these relations
+  * are mutually exclusive and exhaustive.
+  *
+  * Use `Relation.relate` to determine the relation between two [[Interval]]s.
+ **/
 sealed trait Relation
 object Relation {
-  case object After        extends Relation
-  case object Before       extends Relation
-  case object Contains     extends Relation
-  case object During       extends Relation
-  case object Equal        extends Relation
-  case object FinishedBy   extends Relation
-  case object Finishes     extends Relation
-  case object Meets        extends Relation
-  case object MetBy        extends Relation
+
+  /**
+    * 'Interval' `x` is before 'Interval' `y`.
+    *
+    * {{{
+    * 'greater' x '<' 'lesser' y
+    * }}}
+    *
+    * {{{
+    *  +---+
+    *  | x |
+    *  +---+
+    *        +---+
+    *        | y |
+    *        +---+
+    * }}}
+    */
+  case object Before extends Relation
+
+  /**
+    * 'Interval' `x` meets 'Interval' `y`.
+    *
+    * {{{
+    * 'greater' x '==' 'lesser' y
+    * }}}
+    *
+    * {{{
+    *  +---+
+    *  | x |
+    *  +---+
+    *      +---+
+    *      | y |
+    *      +---+
+    * }}}
+    */
+  case object Meets extends Relation
+
+  /**
+    * 'Interval' `x` overlaps 'Interval' `y`.
+    *
+    * {{{
+    * 'lesser' x '<' 'lesser' y '&&'
+    * 'greater' x '' 'lesser' y '&&'
+    * 'greater' x '<' 'greater' y
+    * }}}
+    *
+    * {{{
+    *  +---+
+    *  | x |
+    *  +---+
+    *    +---+
+    *    | y |
+    *    +---+
+    * }}}
+    */
+  case object Overlaps extends Relation
+
+  /**
+    * 'Interval' `x` is finished by 'Interval' `y`.
+    *
+    * {{{
+    * 'lesser' x '<' 'lesser' y '&&'
+    * 'greater' x '==' 'greater' y
+    * }}}
+    *
+    * {{{
+    *  +-----+
+    *  |  x  |
+    *  +-----+
+    *    +---+
+    *    | y |
+    *    +---+
+    * }}}
+    */
+  case object FinishedBy extends Relation
+
+  /**
+    * 'Interval' `x` contains 'Interval' `y`.
+    *
+    * {{{
+    * 'lesser' x '<' 'lesser' y '&&'
+    * 'greater' x '' 'greater' y
+    * }}}
+    *
+    * {{{
+    *  +-------+
+    *  |   x   |
+    *  +-------+
+    *    +---+
+    *    | y |
+    *    +---+
+    * }}}
+    */
+  case object Contains extends Relation
+
+  /**
+    * 'Interval' `x` starts 'Interval' `y`.
+    *
+    * {{{
+    * 'lesser' x '==' 'lesser' y '&&'
+    * 'greater' x '<' 'greater' y
+    * }}}
+
+    * {{{
+    *  +---+
+    *  | x |
+    *  +---+
+    *  +-----+
+    *  |  y  |
+    *  +-----+
+    * }}}
+    */
+  case object Starts extends Relation
+
+  /**
+    * 'Interval' `x` is equal to 'Interval' `y`.
+    *
+    * {{{
+    * 'lesser' x '==' 'lesser' y '&&'
+    * 'greater' x '==' 'greater' y
+    * }}}
+    *
+    * {{{
+    *  +---+
+    *  | x |
+    *  +---+
+    *  +---+
+    *  | y |
+    *  +---+
+    * }}}
+    */
+  case object Equal extends Relation
+
+  /**
+    * 'Interval' `x` is started by 'Interval' `y`.
+    *
+    * {{{
+    * 'lesser' x '==' 'lesser' y '&&'
+    * 'greater' x '' 'greater' y
+    * }}}
+    *
+    * {{{
+    *  +-----+
+    *  |  x  |
+    *  +-----+
+    *  +---+
+    *  | y |
+    *  +---+
+    * }}}
+    */
+  case object StartedBy extends Relation
+
+  /**
+    * 'Interval' `x` is during 'Interval' `y`.
+    *
+    * {{{
+    * 'lesser' x '' 'lesser' y '&&'
+    * 'greater' x '<' 'greater' y
+    * }}}
+    *
+    * {{{
+    *    +---+
+    *    | x |
+    *    +---+
+    *  +-------+
+    *  |   y   |
+    *  +-------+
+    * }}}
+    */
+  case object During extends Relation
+
+  /**
+    * 'Interval' `x` finishes 'Interval' `y`.
+    *
+    * {{{
+    * 'lesser' x '' 'lesser' y '&&'
+    * 'greater' x '==' 'greater' y
+    * }}}
+    *
+    * {{{
+    *    +---+
+    *    | x |
+    *    +---+
+    *  +-----+
+    *  |  y  |
+    *  +-----+
+    * }}}
+    */
+  case object Finishes extends Relation
+
+  /**
+    * 'Interval' `x` is overlapped by 'Interval' `y`.
+    *
+    * {{{
+    * 'lesser' x '' 'lesser' y '&&'
+    * 'lesser' x '<' 'greater' y '&&'
+    * 'greater' x '' 'greater' y
+    * }}}
+    *
+    *    +---+
+    *    | x |
+    *    +---+
+    *  +---+
+    *  | y |
+    *  +---+
+    * }}}
+    */
   case object OverlappedBy extends Relation
-  case object Overlaps     extends Relation
-  case object StartedBy    extends Relation
-  case object Starts       extends Relation
+
+  /**
+    * 'Interval' `x` is met by 'Interval' `y`.
+    *
+    * {{{
+    * 'lesser' x '==' 'greater' y
+    * }}}
+    *
+    *      +---+
+    *      | x |
+    *      +---+
+    *  +---+
+    *  | y |
+    *  +---+
+    * }}}
+    */
+  case object MetBy extends Relation
+
+  /**
+    * 'Interval' `x` is after 'Interval' `y`.
+    *
+    * {{{
+    * 'lesser' x '' 'greater' y
+    * }}}
+    *
+    * {{{
+    *        +---+
+    *        | x |
+    *        +---+
+    *  +---+
+    *  | y |
+    *  +---+
+    * }}}
+    */
+  case object After extends Relation
 
   val values = Seq(
     After,
@@ -32,6 +269,35 @@ object Relation {
     Starts
   )
 
+  /**
+    * Inverts a [[Relation]]. Every 'Relation' has an inverse.
+    *
+    * {{{
+    * invert(Before)       == After
+    * invert(After)        == Before
+    * invert(Meets)        == MetBy
+    * invert(MetBy)        == Meets
+    * invert(Overlaps)     == OverlappedBy
+    * invert(OverlappedBy) == Overlaps
+    * invert(Starts)       == StartedBy
+    * invert(StartedBy)    == Starts
+    * invert(Finishes)     == FinishedBy
+    * invert(FinishedBy)   == Finishes
+    * invert(Contains)     == During
+    * invert(During)       == Contains
+    * invert(Equal)        == Equal
+    * }}}
+    *
+    * Inverting a Relation twice will return the original Relation.
+    * {{{
+    * invert(invert(r)) == r
+    * }}}
+    *
+    * Inverting a Relation is like swapping the arguments to [[Interval#relate]].
+    * {{{
+    * invert(relate(x,y)) == relate(y,x)
+    * }}}
+   **/
   def invert(relation: Relation): Relation =
     relation match {
       case After        => Before
